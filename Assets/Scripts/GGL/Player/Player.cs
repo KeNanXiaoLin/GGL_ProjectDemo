d1b595305672b7605ff484de60f469b0bc491ba1
KeNanXiaoLin <yxl2469a@gmail.com>
2026-02-02 20:52:11 +0800

    测试能否提交提主分支

--- Assets/Scripts/GGL/Player/Player.cs
+++ Assets/Scripts/GGL/Player/Player.cs
@@ -49,7 +49,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
-
+        Debug.Log("Player Awake");  
     }
 
     private void Start()
