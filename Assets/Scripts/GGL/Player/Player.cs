7de59672eae81933615eda99b6d091131ce5175e
KeNanXiaoLin <yxl2469a@gmail.com>
2026-02-04 00:33:31 +0800

    完成表里世界切换

--- Assets/Scripts/GGL/Player/Player.cs
+++ Assets/Scripts/GGL/Player/Player.cs
@@ -42,6 +42,7 @@ public class Player : MonoBehaviour
             case E_World.In_World:
                 ResLoadMgr.Instance.LoadRes<Sprite>(selfMaskData.resSoul, (sprite) =>
                 {
+                    Debug.Log("加载灵魂成功"+selfMaskData.resSoul);
                     soulSR.sprite = sprite;
                 });
                 break;
