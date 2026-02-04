abcc0c6be0ccccff25c4b23c326b9097fb02414f
KeNanXiaoLin <yxl2469a@gmail.com>
2026-02-04 10:48:14 +0800

    添加后处理的效果

--- Assets/Scripts/GGL/Utility/Setting.cs
+++ Assets/Scripts/GGL/Utility/Setting.cs
@@ -6,5 +6,4 @@ public class Setting
 {
     public const string START_SCENE_NAME = "Start Scenes";
     public const string GAME_SCENE_NAME = "GameScene 3";
-    public const string END_SCENE_NAME = "End Scene";
 }
