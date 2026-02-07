2b62deb6d592ebd07add87c5074af1d56346fa8d
KeNanXiaoLin <yxl2469a@gmail.com>
2026-02-07 15:14:33 +0800

    完成路灯的逻辑

--- Assets/Scripts/GGL/MaskAction/Streetlight.cs
+++ Assets/Scripts/GGL/MaskAction/Streetlight.cs
@@ -11,5 +11,28 @@ public class Streetlight : BaseAction
     protected override void DoSelfSpecial()
     {
         // 子类可以重写这个方法，实现自己的特殊表现
+        List<BaseAction> currentActions = GameManager.Instance.GameLogic.actions;
+        foreach (BaseAction action in currentActions)
+        {
+            // 计算距离
+            float distance = Vector3.Distance(transform.position, action.transform.position);
+            
+            // 只影响在checkDis范围内的实体
+            if (distance > data.checkDis)
+                continue;
+            
+            // 路灯会吸引乌鸦朝自己方向移动
+            if (action is Crow)
+            {
+                Vector3 dir = (transform.position - action.transform.position).normalized;
+                action.MoveTo(transform.position - dir);
+            }
+            // 路灯会驱逐老鼠朝自己方向的反方向移动
+            else if (action is Mouse)
+            {
+                Vector3 dir = (action.transform.position - transform.position).normalized;
+                action.MoveTo(transform.position + dir * (data.checkDis + 1));
+            }
+        }
     }
 }
